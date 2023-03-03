b17b7e4f4a12ac297220276ac2051e5655c42d5d
CASSIO <cassio.bjhones@gmail.com>
2023-03-03 03:05:50 -0300

    iff

--- Aulas_Arrays/src/App.java
+++ Aulas_Arrays/src/App.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        String[] consoantes = new String[6];
+        int quantidadeConsoantes = 0;
+
+        int count = 0;
+        do{
+            System.out.print("Letra: ");
+            String letra = scan.next();
+
+            if (!(letra.equalsIgnoreCase("a") |
+                letra.equalsIgnoreCase("e") | 
+                letra.equalsIgnoreCase("i") |
+                letra.equalsIgnoreCase("o") |
+                letra.equalsIgnoreCase("u")) ){
+                    consoantes[count] = letra;
+                    quantidadeConsoantes ++;
+                }
+
+                count++;
+
+        } while(count < consoantes.length);
+
+       
+
+
     }
 }
+// leia um vetor de 6 caracteres
+// e diga quantas consoantes foram lidas
+// imorima as consoantes
