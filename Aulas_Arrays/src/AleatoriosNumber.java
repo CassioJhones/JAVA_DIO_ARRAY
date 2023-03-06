b5bf844cc7e0dacae4d29cba5dd125fab0acabff
CASSIO <cassio.bjhones@gmail.com>
2023-03-06 13:35:20 -0300

    Aleatotios_gerados

--- Aulas_Arrays/src/AleatoriosNumber.java
+++ Aulas_Arrays/src/AleatoriosNumber.java
@@ -1,12 +1,29 @@
 import java.util.Random;
-import java.util.Scanner;
 public class AleatoriosNumber {
     public static void main(String[] args) {
         Random random = new Random();
 
         int[] numerosAleatorios = new int[20];
 
-        for( ; ; )
+            for( int i = 0; i < numerosAleatorios.length; i++ ){
+                int numero = random.nextInt(100);
+                numerosAleatorios[i] = numero;
+            }
+
+            System.out.print("\n--Numeros Aletorios: ");
+            for (int numero : numerosAleatorios) {
+                System.out.print(numero + " "); 
+            }
+
+            System.out.print("\n--Numeros Antecessores: ");
+            for (int numero : numerosAleatorios) {
+                System.out.print((numero-1) + " "); 
+            }
+
+            System.out.print("\n--Numeros Sucessores: ");
+            for (int numero : numerosAleatorios) {
+                System.out.print((numero+1) + " ");  
+            }
     }
 }
 
