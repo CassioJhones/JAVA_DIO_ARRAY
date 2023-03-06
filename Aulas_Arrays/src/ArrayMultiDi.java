3d1341c4cb8641092e2c2e0be83bae2281e31110
CASSIO <cassio.bjhones@gmail.com>
2023-03-06 14:20:37 -0300

    Comments

--- Aulas_Arrays/src/ArrayMultiDi.java
+++ Aulas_Arrays/src/ArrayMultiDi.java
@@ -3,8 +3,11 @@ import java.util.Random;
 public class ArrayMultiDi {
     public static void main(String[] args) {
         Random random = new Random();
+        //--CRIAÇÃO ARRAY 4X4 DE INTEIROS
         int[][] arrayMulti = new int[4][4];
-        
+
+        //--PRA CADA LINHA ELE PERCORRE AS COLUNAS
+        //-- i = linha, j = coluna
         for (int i = 0; i < arrayMulti.length; i++) {
             for (int j = 0; j < arrayMulti[i].length; j++) {
                 arrayMulti[i][j] = random.nextInt(9);
@@ -16,7 +19,7 @@ public class ArrayMultiDi {
                 System.out.print(coluna + " ");
             }
 
-            System.out.println();
+            System.out.println(); //--PULA LINHA
         }
     }
 }
