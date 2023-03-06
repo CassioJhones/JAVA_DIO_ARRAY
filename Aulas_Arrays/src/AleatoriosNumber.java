71d8705044438fe866cd5e810d039e24f5cc65b0
CASSIO <cassio.bjhones@gmail.com>
2023-03-06 13:16:00 -0300

    ok

--- Aulas_Arrays/src/AleatoriosNumber.java
+++ Aulas_Arrays/src/AleatoriosNumber.java
@@ -1,5 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
 public class AleatoriosNumber {
     public static void main(String[] args) {
-        
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for( ; ; )
     }
 }
+
+
+/*
+ PROGRAMA QUE LEIA 20 NUMEROS INTEIROS ALEATORIOS E GUARDE NUM VETOR.
+ AO FINAL, MOSTRE OS NUMEROS E SEUS SUCESSORES.
+ */
