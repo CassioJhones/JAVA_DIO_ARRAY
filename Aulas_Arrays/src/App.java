1aecf059d2a23708682036fc8ba8cfcbb6d729fa
Cassio Jhones <cassio.bjhones@gmail.com>
2023-03-06 14:46:51 -0300

    Update App.java

--- Aulas_Arrays/src/App.java
+++ Aulas_Arrays/src/App.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
@@ -6,21 +7,30 @@ public class App {
         String[] consoantes = new String[6];
         int quantidadeConsoantes = 0;
         int count = 0;
+
+
         
-        do{
+        do {
             System.out.print("Letra: ");
             String letra = scan.next();
 
             if (!(letra.equalsIgnoreCase("a") |
-                letra.equalsIgnoreCase("e") | 
-                letra.equalsIgnoreCase("i") |
-                letra.equalsIgnoreCase("o") |
-                letra.equalsIgnoreCase("u")) ){
-                    consoantes[count] = letra;
-                    quantidadeConsoantes ++;
-                }
-                count++;
-        } while(count < consoantes.length);
+                    letra.equalsIgnoreCase("e") |
+                    letra.equalsIgnoreCase("i") |
+                    letra.equalsIgnoreCase("o") |
+                    letra.equalsIgnoreCase("u"))) {
+                consoantes[count] = letra;
+                quantidadeConsoantes++;
+            }
+            count++;
+        } while (count < consoantes.length);
+
+        System.out.println("-Consoantes: ");
+        for (String consoante : consoantes) {
+            if (consoante != null)
+                System.out.print(consoante + " ");
+        }
+        System.out.println("Quantidade de consoantes: " + quantidadeConsoantes);
     }
 }
 // leia um vetor de 6 caracteres
