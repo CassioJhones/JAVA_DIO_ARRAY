1b674f0043f39a0ffd1386452fcc707162945083
Cassio Jhones <cassio.bjhones@gmail.com>
2023-03-07 12:44:53 -0300

    UPDATE_CONSOANTES

--- Aulas_Arrays/src/App.java
+++ Aulas_Arrays/src/App.java
@@ -7,11 +7,10 @@ public class App {
         String[] consoantes = new String[6];
         int quantidadeConsoantes = 0;
         int count = 0;
-
-
         
+        System.out.print("\n-LETRA-");
         do {
-            System.out.print("Letra: ");
+            System.out.print(" :");
             String letra = scan.next();
 
             if (!(letra.equalsIgnoreCase("a") |
@@ -25,12 +24,12 @@ public class App {
             count++;
         } while (count < consoantes.length);
 
-        System.out.println("-Consoantes: ");
+        System.out.println("\n-Consoantes: ");
         for (String consoante : consoantes) {
             if (consoante != null)
-                System.out.print(consoante + " ");
+                System.out.println(consoante + " ");
         }
-        System.out.println("Quantidade de consoantes: " + quantidadeConsoantes);
+        System.out.println("Quantidade de consoantes: " + quantidadeConsoantes + "\n");
     }
 }
 // leia um vetor de 6 caracteres
